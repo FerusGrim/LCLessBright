b8f797912fb6f5b6de7b2f043046895ec08e414b
Nicholas Badger <awildferus@gmail.com>
2024-03-22 17:21:01 -0400

    fix bad targetDll path in packager, delete existing files
    Signed-off-by: Nicholas Badger <awildferus@gmail.com>

--- scripts/package.bat
+++ scripts/package.bat
@@ -4,13 +4,17 @@ set version=1.0.0
 
 set scriptDir=%~dp0
 set rootDir=%scriptDir%..
-set targetDll=%rootDir%\LessBright\bin\Release\net48\LessBright.dll
+set targetDll=%rootDir%\bin\Release\net48\LessBright.dll
 set destDir=%rootDir%\package
 set zipDest=%rootDir%\package\LessBright-%version%.zip
+
+if exist %destDir%\LessBright.dll del %destDir%\LessBright.dll
 
 echo Deploying: %targetDll%
 echo Destination: %destDir%
 copy %targetDll% %destDir%
+
+if exist %zipDest% del %zipDest%
 
 echo Zipping: %zipDest%
 powershell Compress-Archive -Path %destDir%\* -DestinationPath %zipDest%
